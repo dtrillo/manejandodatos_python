36249fd142a26c3f3b873839f480ebf4359f27cb
David Trillo <dtrillo@gmail.com>
2014-10-14 16:50:30 +0200

    Ordena una lista

---
+++ ordena_lista_objetos_01.py
@@ -0,0 +1,7 @@
+#-------------------------------------------------------------------------------
+# Copyright:   (c)Manejando datos 2014
+#-------------------------------------------------------------------------------
+miVersion = "Ordenación de objetos con Python - version 0.1.0"
+
+milista = [4,1,8,2,9]
+print sorted(milista)
